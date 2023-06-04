76fa8d53a2b087beaef0a76e95be93db6a47d09e
Jonao6 <xeduardojonatas@gmail.com>
2023-06-04 15:56:05 +0000

    push to my github

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,30 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
 
+const upload = multer({ storage: storage });
+
+
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  const name = req.file.originalname
+  const type = req.file.mimetype
+  const size = req.file.size
+
+  res.json({
+    name: name,
+    type: type,
+    size: size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
